3666b614d4019003bf041ea093cdd049f87b78d5
Steve Paul <stevep@interrait.com>
2024-01-09 21:37:14 +0530

    Fix cors errors

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

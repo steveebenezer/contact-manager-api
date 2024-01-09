c5057fa2b079f4932a667e460c2245c7e2078ca8
Steve Paul <stevep@interrait.com>
2024-01-09 18:48:08 +0530

    Add contact interface model

---
+++ Models/Contact.cs
@@ -0,0 +1,10 @@
+namespace ContactManagerApi.Models
+{
+  public class Contact
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+  }
+}
